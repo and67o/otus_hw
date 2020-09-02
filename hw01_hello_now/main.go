cf5a255b33b4cad837b7641bad6b10fe8457c027
oleg <kenny_67@mail.ru>
2020-09-02 23:04:44 +0300

    hw1 done

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"log"
+	_time "time"
+)
+
+const host = "0.beevik-ntp.pool.ntp.org"
 func main() {
-	// Place your code here
+	currentTime:=_time.Now()
+	time, err := ntp.Time(host)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Printf("current time: %v\n", currentTime)
+	fmt.Printf("exact time: %v\n", time)
 }
