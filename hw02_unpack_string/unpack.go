817c476be7c7de78e08ad5a92c5054bf44e04893
oleg <kenny_67@mail.ru>
2020-09-12 19:41:40 +0300

    hw2 done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,49 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(str string) (string, error) {
+	var resStr strings.Builder
+
+	firstChar, _ := utf8.DecodeRuneInString(str)
+
+	if unicode.IsDigit(firstChar) || haveDoubleNumbers(str) {
+		return "", ErrInvalidString
+	}
+
+	for i := 0; i < len(str); i++ {
+		value := str[i]
+		if !unicode.IsDigit(rune(value)) {
+			nextIndex := i + 1
+			if nextIndex == len(str) {
+				nextIndex = i
+			}
+			if nextIndex < len(str) {
+				nexValue := str[nextIndex]
+				if unicode.IsDigit(rune(nexValue)) {
+					countOfAdd, _ := strconv.Atoi(string(nexValue))
+					strAdd := strings.Repeat(string(value), countOfAdd)
+					resStr.WriteString(strAdd)
+				} else {
+					resStr.WriteString(string(value))
+				}
+			}
+		}
+	}
+	return resStr.String(), nil
+}
+
+func haveDoubleNumbers(str string) bool {
+	reg := regexp.MustCompile("\\d{2,}")
+	doubleNumber := reg.FindString(str)
+
+	return doubleNumber != ""
 }
