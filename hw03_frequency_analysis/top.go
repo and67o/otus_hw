ee2464051a3e269770914e9ba7b1318cc40e778d
oleg <kenny_67@mail.ru>
2020-09-21 00:28:45 +0300

    first

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,45 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
+import (
+	"fmt"
+	"regexp"
+	"sort"
+	"strings"
+)
 
-func Top10(_ string) []string {
-	// Place your code here
+
+
+func Top10(text string) []string {
+	text = handleText(text)
+	textStruct := strings.Split(text, " ")
+	countsWords := countSame(textStruct)
+	sort.Slice(countsWords, func(i, j int) bool {
+		fmt.Println(countsWords[i], countsWords[j])
+		return true
+	})
+
+	//for k, v := range countsWords {
+	//	fmt.Printf("Item : %s --- Count : %d\n", k, v)
+	//}
 	return nil
+}
+
+func handleText(text string) string {
+	text = strings.ToLower(text)
+	space := regexp.MustCompile("\\s+")
+	text = space.ReplaceAllString(text, " ")
+	return text
+}
+
+func countSame(items []string) map[string]int {
+	countSame := make(map[string]int)
+	for _, item := range items {
+		_, ok := countSame[item]
+
+		if ok {
+			countSame[item] += 1
+		} else {
+			countSame[item] = 1
+		}
+	}
+	return countSame
 }
