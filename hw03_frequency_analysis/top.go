1d86ddc974bee0ff2be71a9ca0f4f3bae70dc0c6
oleg <kenny_67@mail.ru>
2020-09-22 23:21:50 +0300

    hw3 done easy part

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,45 +1,69 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 import (
-	"fmt"
 	"regexp"
 	"sort"
 	"strings"
 )
 
+const MaxWords = 10
 
+type countWords struct {
+	word  string
+	count int
+}
 
 func Top10(text string) []string {
 	text = handleText(text)
+	if text == "" {
+		return nil
+	}
 	textStruct := strings.Split(text, " ")
-	countsWords := countSame(textStruct)
-	sort.Slice(countsWords, func(i, j int) bool {
-		fmt.Println(countsWords[i], countsWords[j])
-		return true
-	})
+	countsWords := getCountWords(textStruct)
+	wordsStruct := handleWordsToStruct(countsWords)
+	sortWords(wordsStruct)
+	if len(wordsStruct) > MaxWords {
+		wordsStruct = wordsStruct[0:MaxWords]
+	}
 
-	//for k, v := range countsWords {
-	//	fmt.Printf("Item : %s --- Count : %d\n", k, v)
-	//}
-	return nil
+	return getOnlyWords(wordsStruct)
+}
+
+func getOnlyWords(wordsStruct []countWords) []string {
+	res := make([]string, len(wordsStruct))
+	for _, v := range wordsStruct {
+		res = append(res, v.word)
+		if len(res) == 10 {
+			break
+		}
+	}
+	return res
+}
+
+func sortWords(wordsStruct []countWords) {
+	sort.Slice(wordsStruct, func(i, j int) bool {
+		return wordsStruct[i].count > wordsStruct[j].count
+	})
 }
 
 func handleText(text string) string {
-	text = strings.ToLower(text)
-	space := regexp.MustCompile("\\s+")
+	// text = strings.ToLower(text)
+	space := regexp.MustCompile(`\s+`)
 	text = space.ReplaceAllString(text, " ")
 	return text
 }
 
-func countSame(items []string) map[string]int {
-	countSame := make(map[string]int)
-	for _, item := range items {
-		_, ok := countSame[item]
-
-		if ok {
-			countSame[item] += 1
-		} else {
-			countSame[item] = 1
-		}
+func handleWordsToStruct(items map[string]int) []countWords {
+	wordsStruct := make([]countWords, len(items))
+	for i, v := range items {
+		wordsStruct = append(wordsStruct, countWords{i, v})
 	}
-	return countSame
+	return wordsStruct
+}
+
+func getCountWords(items []string) map[string]int {
+	countWord := make(map[string]int)
+	for _, v := range items {
+		countWord[v]++
+	}
+	return countWord
 }
