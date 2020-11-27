70806b013db7c2ab4f7bb27f2ad788c398c77016
oleg <kenny_67@mail.ru>
2020-11-27 20:48:51 +0300

    hw08 add base

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type test struct {
+	path    string
+	expected Environment
+	err      error
+}
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	for _, tst := range [...]test{
+		{
+			path: "./testdata/env",
+			expected: map[string]string{
+				"BAR":   "bar",
+				"FOO":   "   foo\nwith new line",
+				"HELLO": `"hello"`,
+				"UNSET": "",
+			},
+		},
+	} {
+		result, err := ReadDir(tst.path)
+		require.Equal(t, tst.expected, result)
+		require.Nil(t, tst.err, err)
+	}
 }
