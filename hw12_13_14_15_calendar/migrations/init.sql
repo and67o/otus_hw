8497d466a2f6671edbb3c4045c8a92254336cd05
oleg <kenny_67@mail.ru>
2021-01-17 01:45:01 +0300

    <hw12> add migration

---
+++ hw12_13_14_15_calendar/migrations/init.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE `events` (
+                          `id` varchar(255) NOT NULL,
+                          `title` varchar(67) NOT NULL,
+                          `date` datetime DEFAULT NULL,
+                          `duration` int NOT NULL,
+                          `description` varchar(127),
+                          `owner_id` int NOT NULL,
+                          `notify_before` int NOT NULL,
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE IF EXISTS events;
+
